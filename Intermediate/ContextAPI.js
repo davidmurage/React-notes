18de6039ae47858b123724ba4a9bbe5085d74efe
david Murage <davidmurage415@gmail.com>
2024-02-06 10:35:27 +0300

    updated ContextAPI

--- Intermediate/ContextAPI.js
+++ Intermediate/ContextAPI.js
@@ -56,3 +56,19 @@ const ThemedComponent = () => {
   );
 };
 
+
+{/*The Context API is primarily applied in React applications. It provides a way to pass data through the component tree without having to pass props down manually at every level. 
+Context allows you to share values like themes, localization preferences, 
+or any other global data across many components in a React application without explicitly passing props through every level of the component tree.
+
+You typically use the Context API in scenarios where data needs to be accessible to many components at different nesting levels without the need for prop drilling (passing props through multiple levels of components).
+
+Here are some common use cases for applying the Context API:
+
+1.Theming: You can use context to provide the current theme to all components in a React application.
+2.Authentication: Storing authentication state and making it available to components that need it.
+3.Internationalization (i18n): Storing locale information and providing it to components for translation.
+4.Redux-like state management: While Redux is a more comprehensive solution for state management, in simpler cases, you might use context to manage global state.
+In summary, the Context API in React is applied wherever you need to pass data or state down the component tree to child components without 
+explicitly passing it through props at every level. 
+It's especially useful for providing global data or state that many components across the application might need access to.*/}
